15af73dd44545c390c473cd3c19fa842e3a3ba04
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-06 17:26:42 +0200

    Task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector(".change-color");
+
+const textValueRef = document.querySelector(".color");
+
+buttonRef.addEventListener("click", onChangeColorBody);
+
+function onChangeColorBody() {
+  const colorOfBody = (document.body.style.backgroundColor =
+    getRandomHexColor());
+
+  textValueRef.textContent = colorOfBody;
+}
