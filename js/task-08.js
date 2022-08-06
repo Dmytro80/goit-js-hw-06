3530fd3ad70f3b589d4204a68d4f70ddc376c931
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-06 16:48:15 +0200

    Task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value;
+
+  const password = formElements.password.value;
+
+  const formDate = {
+    email,
+    password,
+  };
+
+  email === "" || password === ""
+    ? alert("Please fill in all the fields")
+    : console.log(formDate);
+
+  form.reset();
+}
